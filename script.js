da9bb892bf764e09a46b19727253fc02ecf61e7a
Kuldeep Pathak <imkuldeep2012@gmail.com>
2024-04-25 10:15:38 +0530

    new version

--- script.js
+++
@@ -1,9 +0,0 @@
-// Get the current page views count from local storage
-let pageViewsCount = localStorage.getItem('pageViews') || 0;
-
-// Update the page views display
-document.getElementById('count').innerText = pageViewsCount;
-
-// Increment the page views count and update local storage on page load
-pageViewsCount++;
-localStorage.setItem('pageViews', pageViewsCount);
